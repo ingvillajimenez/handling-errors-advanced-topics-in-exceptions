86933740c23aa2cbfa5f63ed90f322e7a890bc10
Juan Villa <ing.villa.jimenez@gmail.com>
2023-07-21 11:33:35 -0600

    Chaining Exceptions

--- JavaExceptionHandling/src/ThrowExceptions.java
+++ JavaExceptionHandling/src/ThrowExceptions.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import javax.naming.InvalidNameException;
+import java.util.InputMismatchException;
+import java.security.InvalidParameterException;
 
 public class ThrowExceptions {
 
@@ -10,32 +12,44 @@ public class ThrowExceptions {
         System.out.print("Enter your username: ");
         String uname = inputStudent.next();
 
-        System.out.print("Enter your GPA: ");
-        float gpa = inputStudent.nextFloat();
+//        System.out.print("Enter your GPA: ");
+//        float gpa = inputStudent.nextFloat();
+
+        float gpa = 0f;
+
+        try {
+            System.out.print("Enter your GPA: ");
+            gpa = inputStudent.nextFloat(); // java.util.InputMismatchException
+        }
+        catch (InputMismatchException ex) {
+            gpa = 1.0f;
+
+            ex.initCause(new IllegalArgumentException("The GPA needs to be a number between 0 and 4.33."));
+
+            System.out.println("What is the cause of the Exception? \n" + ex.getCause());
+        }
+//        catch (InputMismatchException ex) {
+//            throw new IllegalArgumentException("A GPA must have a numeric value between 0 and 4.33.");
+//        }
 
         inputStudent.close();
 
-//        validateStudent(uname, gpa);
-
-//        try {
-//            intermediateFunction(uname, gpa);
-//        } catch (IllegalArgumentException | InvalidNameException e) {
-//
-//            System.out.println("An exception was thrown: " + e.getClass());
-//            e.printStackTrace();
-//        }
-
-        intermediateFunction(uname, gpa);
+        try {
+            intermediateFunction(uname, gpa); // javax.naming.InvalidNameException
+        }
+        catch (InvalidNameException e) {
+            System.out.println(e.getClass() + ": The username is invalid");
+            e.printStackTrace();
+        }
     }
 
-//    public static void intermediateFunction(String userId, float gpa)
-//            throws InvalidNameException, IllegalArgumentException {
-    public static void intermediateFunction(String userId, float gpa) {
+    public static void intermediateFunction(String userId, float gpa)
+            throws InvalidNameException {
         try {
-            validateStudent(userId, gpa);
-        } catch (IllegalArgumentException | InvalidNameException e) {
+            validateStudent(userId, gpa); // java.lang.IllegalArgumentException
+        } catch (IllegalArgumentException e) {
 
-            System.out.println("An exception was thrown: " + e.getClass());
+            System.out.println(e.getClass() + ": The GPA is invalid");
             e.printStackTrace();
         }
     }
@@ -51,7 +65,7 @@ public class ThrowExceptions {
         }
 
         if (gpa > 0 && gpa <= 4.33) {
-            System.out.println("Tha is a valid GPA. Processing eligibility...");
+            System.out.println("That is a valid GPA. Processing eligibility...");
         }
         else {
             throw new IllegalArgumentException("Sorry. A GPA must have a value between 0 and 4.33.");
